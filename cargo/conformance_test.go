0e34c256a14afb26e65db4fe8956c32bc4e8fb5b
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-11-15 19:07:13 -0600

    Add initial tests and test fixtures
    This commit adds tests to the cargo module. Both the initial unit tests
    as well as some conformance verification is done. The conformance part
    needs a rework as we will need to make them work  the same across all
    ecosystems but for now we have some initial ones.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ cargo/conformance_test.go
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package cargo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetRootModule(t *testing.T) {
+	mod := New()
+	pkg, err := mod.GetRootModule("./test_data/")
+	require.NoError(t, err)
+	require.NotNil(t, pkg)
+	require.Equal(t, "hello-server", pkg.Name)
+	require.Equal(t, "Apache-2.0", pkg.LicenseDeclared)
+	// TODO: Add more fields as they become available
+}
+
+func TestGetMetadata(t *testing.T) {
+	mod := New()
+
+	err := mod.SetRootModule("./test_data/")
+	metadata := mod.GetMetadata()
+	require.NoError(t, err)
+	require.NotNil(t, metadata)
+	require.Equal(t, "cargo", metadata.Slug)
+	require.Equal(t, "Cargo.toml", metadata.Manifest[0])
+}
+func TestListUsedModules(t *testing.T) {
+	mod := New()
+	mods, err := mod.ListUsedModules("./test_data/")
+	require.NoError(t, err)
+	require.Len(t, mods, 3, "No jala salieron %s", len(mods))
+}
+
+func TestListModulesWithDeps(t *testing.T) {
+	mod := New()
+	mods, err := mod.ListModulesWithDeps("./test_data/", "")
+	require.NoError(t, err)
+	require.Len(t, mods, 3, len(mods))
+	for _, p := range mods {
+		switch p.Name {
+		case "hyper":
+			require.Lenf(t, p.Packages, 20, "%s has %d deps", p.Name, len(p.Packages))
+		case "pretty_env_logger":
+			require.Lenf(t, p.Packages, 2, "%s has %d deps", p.Name, len(p.Packages))
+		case "tokio":
+			require.Lenf(t, p.Packages, 12, "%s has %d deps", p.Name, len(p.Packages))
+		default:
+			t.Error("Unexpected name: " + p.Name)
+		}
+	}
+	require.Len(t, mods, 3)
+}
