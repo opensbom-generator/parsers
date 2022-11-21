498e8f572063e982c603fb2d142884691419d2c9
Nisha Kumar <nisha.kumar@oracle.com>
2022-11-21 07:01:22 -0800

    npm: Add package representations for v2
    Signed-off-by: Nisha Kumar <nisha.kumar@oracle.com>

--- npm/reader.go
+++ npm/reader.go
@@ -8,10 +8,6 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
-
-	"github.com/spdx/tools-golang/spdx/v2_1"
-	"github.com/spdx/tools-golang/spdx/v2_2"
-	"github.com/spdx/tools-golang/spdx/v2_3"
 )
 
 // List of known manifest files in order of priority
@@ -50,48 +46,26 @@ func ReadManifest(manifest_file string) (map[string]interface{}, error) {
 		return nil, errors.New("Cannot unmarshal JSON data")
 	}
 	return data, nil
-
 }
 
-// MakeSpdxObject instantiates an SPDX object based on the provided SPDX version
-func MakeSpdxDocVersion(spdx_version string) (interface{}, error) {
-	switch spdx_version {
-	case "2.1":
-		return v2_1.Document, nil
-	case "2.2":
-		return v2_2.Document, nil
-	case "2.3":
-		return v2_3.Document, nil
-	default:
-		return nil, errors.New("Unknown or unsupported SPDX Document version")
-	}
-}
+// ParseManifestV2 converts a map[string]interface{} object into
+// a struct representing the NPM v2 lockfile
+func ParseManifestV2(data map[string]interface{}) (PackageLockV2, error) {
+	// PackageV2 and PackageLockV2 come from model.go
+	lock := PackageLockV2
+	// fill in Name, Version and LockfileVersion
+	PackageLockV2.Name = data["name"]
+	PackageLockV2.Version = data["version"]
+	PackageLockV2.LockfileVersion = data["lockfileVersion"]
+	PackageLockV2.Requires = data["requires"]
+	// fill in Packages
+	packages := data["packages"].(map[string]interface{})
+	for pkg_name, pkg_obj := range packages {
 
-// ManifestV2ToSpdx converts manifest data conforming to npm lockfile version 2
-// to an SPDX object of a given version.
-// A generic interface is provided to support the different SPDX objects
-func ManifestV2ToSpdx(spdx_version string, data map[string]interface{}) (interface{}, error) {
-	spdx_doc, err := MakeSpdxDocVersion(spdx_version)
-	if err != nil {
-		return nil, err
 	}
-	//
-}
+	// fill in Dependencies
+	dependencies := data["dependencies"].(map[string]interface{})
+	for dep_name, dep_obj := range dependencies {
 
-// GetSpdxDoc detects a manifest file in the given project path.
-// Depending on what the spdx_version is, the corresponding SPDX
-// model is created, filled in, and returned.
-func GetSpdxDoc(proj_path, spdx_version string) (interface{}, error) {
-	// detect manifest existing in the path
-	manifest_file := DetectManifest(proj_path)
-	if manifest_file == nil {
-		// no manifest file detected, so return an error
-		return nil, errors.New("No npm manifest file detected")
 	}
-	// read the manifest file
-	content, err := ReadManifest(manifest_file)
-	if err != nil {
-		return nil, err
-	}
-
 }
