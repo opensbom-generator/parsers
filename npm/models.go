b9d6c5469e96d6de2b271491b98058250118e970
Nisha Kumar <nisha.kumar@oracle.com>
2022-12-08 09:12:15 -0800

    npm: Added reader to parse npm V2 manifest
    - Changed the "model.go" file to "models.go" to indicate it being
      used for multiple manifest models.
    - Completed the ParseManifestV2 function to parse a NPM Lockfile
      version 2 lock manifest.
    - Added a test for ParseManifestV2 function. This test can be
      run independent of the other test using:
      `go test -v reader_test.go reader.go models.go`
    
    Signed-off-by: Nisha Kumar <nisha.kumar@oracle.com>

---
+++ npm/models.go
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022, Oracle and/or its affiliates.
+
+package npm
+
+// PackageV2 represents the NPM V2 lockfile's
+// package object.
+type PackageV2 struct {
+	Version          string
+	Resolved         string
+	Integrity        string
+	Dev              bool
+	Engines          map[string]string
+	Dependencies     map[string]string
+	Bin              map[string]string
+	Deprecated       string
+	HasInstallScript bool
+}
+
+// RootPackageV2 represents the NPM V2 lockfile's
+// root package object.
+type RootPackageV2 struct {
+	PackageV2
+	Name            string
+	License         string
+	DevDependencies map[string]string
+}
+
+// PackageLockV2 represents the NPM v2 lockfile
+type PackageLockV2 struct {
+	Name            string
+	Version         string
+	LockfileVersion int
+	Requires        bool
+	RootPackage     RootPackageV2
+	Packages        map[string]PackageV2
+}
